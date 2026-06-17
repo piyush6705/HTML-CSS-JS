094fedb68dedde5ffaf2d44bd00278d3d8957040
piyush6705 <sharmapiyush6705@gmail.com>
2026-06-17 23:09:15 +0530

    removed filler code

--- amazon-projoect/scripts/checkout/orderSummary.js
+++ amazon-projoect/scripts/checkout/orderSummary.js
@@ -6,9 +6,7 @@ import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 
-const today =dayjs();
-const deliveryDate=today.add(7,'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+
 
 export function renderOrderSummary(){
 

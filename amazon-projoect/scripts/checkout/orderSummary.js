459defe60f91626d9f7e370a229100c160587c51
piyush6705 <sharmapiyush6705@gmail.com>
2026-06-18 19:02:43 +0530

    failed test

--- amazon-projoect/scripts/checkout/orderSummary.js
+++ amazon-projoect/scripts/checkout/orderSummary.js
@@ -63,7 +63,7 @@ export function renderOrderSummary(){
               <span class="update-quantity-link link-primary">
                 Update
               </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>

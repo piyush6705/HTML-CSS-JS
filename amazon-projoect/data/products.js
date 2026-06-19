c00127db224e076bd6f73ccee37c9fd78e94bcdb
piyush6705 <sharmapiyush6705@gmail.com>
2026-06-19 21:14:42 +0530

    created clothinng class and added size chart

--- amazon-projoect/data/products.js
+++ amazon-projoect/data/products.js
@@ -34,8 +34,28 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
 
+class Clothing extends Product{ 
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+      Size Chart
+      </a>
+    `;
+  }
+}
 
 
 
@@ -699,6 +719,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 }); 
 

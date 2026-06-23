d506a34d614be5ebcd54f293162488529739d2ca
piyush6705 <sharmapiyush6705@gmail.com>
2026-06-23 21:27:23 +0530

    created new script file for order

---
+++ amazon-projoect/scripts/orders.js
@@ -0,0 +1,6 @@
+
+import {getProduct, loadProductsFetch} from '../data/products.js';
+import {orders} from '../data/orders.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import formatCurrency from './utils/money.js';
+

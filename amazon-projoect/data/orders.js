9bc2bb4953d620e1de604867eb5522016bb41656
piyush6705 <sharmapiyush6705@gmail.com>
2026-06-23 22:55:14 +0530

    added html to tracking.js

--- amazon-projoect/data/orders.js
+++ amazon-projoect/data/orders.js
@@ -1,4 +1,3 @@
-
 export const orders = JSON.parse(localStorage.getItem('orders')) || [];
 
 export function addOrder(order) {
@@ -10,3 +9,14 @@ function saveToStorage() {
   localStorage.setItem('orders', JSON.stringify(orders));
 }
 
+export function getOrder(orderId) {
+  let matchingOrder;
+
+  orders.forEach((order) => {
+    if (order.id === orderId) {
+      matchingOrder = order;
+    }
+  });
+
+  return matchingOrder;
+}

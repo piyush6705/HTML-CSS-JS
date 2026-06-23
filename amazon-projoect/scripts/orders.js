60707d9d9e440b6ee0bbc8f9f56b2b7315015a22
piyush6705 <sharmapiyush6705@gmail.com>
2026-06-23 22:57:56 +0530

    made trankking page and order page interacrive

--- amazon-projoect/scripts/orders.js
+++ amazon-projoect/scripts/orders.js
@@ -2,6 +2,7 @@ import {getProduct, loadProductsFetch} from '../data/products.js';
 import {orders} from '../data/orders.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import formatCurrency from './utils/money.js';
+import {addToCart} from '../data/cart.js';
 
 async function loadPage() {
   await loadProductsFetch();
@@ -9,11 +10,7 @@ async function loadPage() {
   let ordersHTML = '';
 
   orders.forEach((order) => {
-  if (!order.products) {
-    return;
-  }
-
-  const orderTimeString = dayjs(order.orderTime).format('MMMM D');
+    const orderTimeString = dayjs(order.orderTime).format('MMMM D');
 
     ordersHTML += `
       <div class="order-container">
@@ -43,7 +40,6 @@ async function loadPage() {
   });
 
   function productsListHTML(order) {
-
     let productsListHTML = '';
 
     order.products.forEach((productDetails) => {
@@ -66,7 +62,8 @@ async function loadPage() {
           <div class="product-quantity">
             Quantity: ${productDetails.quantity}
           </div>
-          <button class="buy-again-button button-primary">
+          <button class="buy-again-button button-primary js-buy-again"
+            data-product-id="${product.id}">
             <img class="buy-again-icon" src="images/icons/buy-again.png">
             <span class="buy-again-message">Buy it again</span>
           </button>
@@ -86,6 +83,21 @@ async function loadPage() {
   }
 
   document.querySelector('.js-orders-grid').innerHTML = ordersHTML;
+
+  document.querySelectorAll('.js-buy-again').forEach((button) => {
+    button.addEventListener('click', () => {
+      addToCart(button.dataset.productId);
+
+      
+      button.innerHTML = 'Added';
+      setTimeout(() => {
+        button.innerHTML = `
+          <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+        `;
+      }, 1000);
+    });
+  });
 }
 
 loadPage();
--- amazon-projoect/scripts/tracking.js
+++ amazon-projoect/scripts/tracking.js
@@ -12,14 +12,18 @@ async function loadPage() {
   const order = getOrder(orderId);
   const product = getProduct(productId);
 
-  // Get additional details about the product like
-  // the estimated delivery time.
+ 
   let productDetails;
   order.products.forEach((details) => {
     if (details.productId === product.id) {
       productDetails = details;
     }
   });
+
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+  const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
 
   const trackingHTML = `
     <a class="back-to-orders-link link-primary" href="orders.html">
@@ -43,19 +47,25 @@ async function loadPage() {
     <img class="product-image" src="${product.image}">
 
     <div class="progress-labels-container">
-      <div class="progress-label">
+      <div class="progress-label ${
+        percentProgress < 50 ? 'current-status' : ''
+      }">
         Preparing
       </div>
-      <div class="progress-label current-status">
+      <div class="progress-label ${
+        (percentProgress >= 50 && percentProgress < 100) ? 'current-status' : ''
+      }">
         Shipped
       </div>
-      <div class="progress-label">
+      <div class="progress-label ${
+        percentProgress >= 100 ? "current-status" : ''
+      }">
         Delivered
       </div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style="width: ${percentProgress}%;"></div>
     </div>
   `;
 

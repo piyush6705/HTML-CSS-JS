be677a061a8eba15ed79faa759de8d24ab32e1e6
piyush6705 <sharmapiyush6705@gmail.com>
2026-06-25 00:22:27 +0530

    adding buttons usinng js and inn style changing correct and incorrect answer colors

--- quiz app/script.js
+++ quiz app/script.js
@@ -1,2 +1,91 @@
+const questions = [
+  {
+    question: "What is the capital of Australia?",
+    answers: [
+      { text: "Sydney", correct: false },
+      { text: "Melbourne", correct: false },
+      { text: "Canberra", correct: true },
+      { text: "Perth", correct: false }
+    ]
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    answers: [
+      { text: "Venus", correct: false },
+      { text: "Mars", correct: true },
+      { text: "Jupiter", correct: false },
+      { text: "Saturn", correct: false }
+    ]
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    answers: [
+      { text: "Vincent van Gogh", correct: false },
+      { text: "Pablo Picasso", correct: false },
+      { text: "Leonardo da Vinci", correct: true },
+      { text: "Michelangelo", correct: false }
+    ]
+  },
+  {
+    question: "Which is the largest ocean on Earth?",
+    answers: [
+      { text: "Atlantic Ocean", correct: false },
+      { text: "Indian Ocean", correct: false },
+      { text: "Arctic Ocean", correct: false },
+      { text: "Pacific Ocean", correct: true }
+    ]
+  }
+];
 
-const q
+const questionElement =document.getElementById("question");
+const answerButtons =document.getElementById("answer-buttons");
+const nextButton =document.getElementById("next-btn");
+
+let  currentQuestionIndex = 0;
+let score = 0;
+
+
+function startQuix(){
+  currentQuestionIndex =0;
+  score= 0;
+  nextButton.innerHTML='Next';
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion =questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex +1;
+  questionElement.innerHTML =questionNo + ". " + currentQuestion.
+  question;
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if(answer.correct){
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState(){
+  nextButton.style.display = "none";
+  while(answerButtons.firstChild){
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e){
+  const selectedBtn =e.target;
+  const isCorrect = selectedBtn.dataset.correct ==="true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+  }else{
+    selectedBtn.classList.add("incorrect");
+  }
+  
+}
+startQuix();
